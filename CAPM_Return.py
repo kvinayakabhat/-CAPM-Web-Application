ca0a1af250b3b8d16476b9691622213c5b1fdf26
Vinayak <102165372+kvinayakabhat@users.noreply.github.com>
2023-08-23 22:12:03 +0530

    Add files via upload

--- CAPM_Return.py
+++ CAPM_Return.py
@@ -13,7 +13,7 @@ streamlit.set_page_config(page_title="CAPM",
                    page_icon="chart_with_upwards_trend",
                    layout="wide") #to display the screen full wide
 
-streamlit.title("Stock Capital Asset Pricing Model") #title of the website
+streamlit.title("Stock Capital Asset Pricing Model ") #title of the website
 
 #user input
 
@@ -51,10 +51,10 @@ try:
     col1,col2 = streamlit.columns([1,1])
     with col1:
         streamlit.markdown("### DataFrame head")
-        streamlit.dataframe(stocks_df.head(), use_container_width =True)
+        streamlit.dataframe(stocks_df.head(), use_container_width =True) #contains the head value
     with col2:
         streamlit.markdown("### DataFrame tail")
-        streamlit.dataframe(stocks_df.tail(), use_container_width =True)
+        streamlit.dataframe(stocks_df.tail(), use_container_width =True) #contains the tail value
 
 
         #creating a chart
@@ -62,13 +62,13 @@ try:
     col1,col2 =streamlit.columns([1,1])
     with col1:
         streamlit.markdown("### Price of all the stocks")
-        streamlit.plotly_chart(capm_functions.interactive_plot(stocks_df))
+        streamlit.plotly_chart(capm_functions.interactive_plot(stocks_df)) #plotting stocks chart
 
     with col2:
         streamlit.markdown("### Price of all the stocks (After Normalization)")
-        streamlit.plotly_chart(capm_functions.interactive_plot(capm_functions.normalize(stocks_df)))
+        streamlit.plotly_chart(capm_functions.interactive_plot(capm_functions.normalize(stocks_df)))#ploting normalized 
 
-    stocks_daily_return = capm_functions.daily_return(stocks_df)
+    stocks_daily_return = capm_functions.daily_return(stocks_df)  #daily returns print
     print(stocks_daily_return.head())
 
     #create 2 dict
@@ -85,18 +85,18 @@ try:
 
     print (beta, alpha)
 
-    beta_df =pd.DataFrame(columns =['Stock','Beta Value'])
+    beta_df =pd.DataFrame(columns =['Stock','Beta Value'])   #storing
     beta_df['Stock']= beta.keys()
-    beta_df['Beta Value'] = [str(round(i,2)) for i in beta.values()]
+    beta_df['Beta Value'] = [str(round(i,2)) for i in beta.values()]   #calculated values storing
 
     with col1:
         streamlit.markdown('### Calculated Beta Value')
-        streamlit.dataframe(beta_df, use_container_width =True)
+        streamlit.dataframe(beta_df, use_container_width =True)    #beta value print
 
     rf = 0
     rm= stocks_daily_return['sp500'].mean()*252
 
-    return_df = pd.DataFrame()
+    return_df = pd.DataFrame()    # for return values
     return_value =[]
     for stock, value in beta.items():
         return_value.append(str(round(rf+(value*(rm-rf)),2))) #market return minus risk free return
